d1a229a6c5a69f9e03e47591ed1da7f3bd1eeef3
Paul Cunningham <paul@locklan.com.au>
2015-08-27 12:59:42 +1000

    Updates scripts
    Added GetExchangeURLs.ps1 and made updates to ConfigureExchangeURLs.ps1
    for better error handling and added capability to configure multiple
    servers at once.

--- ConfigureExchangeURLs.ps1
+++ ConfigureExchangeURLs.ps1
@@ -4,7 +4,7 @@ ConfigureExchangeURLs.ps1
 
 .DESCRIPTION 
 PowerShell script to configure the Client Access server URLs
-for Microsoft Exchange Server 2013. All Client Access server
+for Microsoft Exchange Server 2013/2016. All Client Access server
 URLs will be set to the same namespace.
 
 If you are using separate namespaces for each CAS service this script will
@@ -15,7 +15,7 @@ different auth requirements for Outlook Anywhere modify that command
 in the script first.
 
 .PARAMETER Server
-The name of the server you are configuring.
+The name(s) of the server(s) you are configuring.
 
 .PARAMETER InternalURL
 The internal namespace you are using.
@@ -23,11 +23,23 @@ The internal namespace you are using.
 .PARAMETER ExternalURL
 The internal namespace you are using.
 
+.PARAMETER DefaultAuth
+The default authentication method to set for Outlook Anywhere. Defaults to NTLM.
+
+.PARAMETER InternalSSL
+Specifies the internal SSL requirement for Outlook Anywhere. Defaults to True (SSL required).
+
+.PARAMETER ExternalSSL
+Specifies the external SSL requirement for Outlook Anywhere. Defaults to True (SSL required).
+
 .EXAMPLE
-.\ConfigureURLs.ps1 -Server ex2013srv1 -InternalURL mail.exchangeserverpro.net -ExternalURL mail.exchangeserverpro.net
+.\ConfigureExchangeURLs.ps1 -Server sydex1 -InternalURL mail.exchangeserverpro.net -ExternalURL mail.exchangeserverpro.net
+
+.EXAMPLE
+.\ConfigureExchangeURLs.ps1 -Server sydex1,sydex2 -InternalURL mail.exchangeserverpro.net -ExternalURL mail.exchangeserverpro.net
 
 .LINK
-http://exchangeserverpro.com/exchange-server-2010-2013-migration-configuring-client-access-servers/
+http://exchangeserverpro.com/powershell-script-configure-exchange-urls/
 
 .NOTES
 Written by: Paul Cunningham
@@ -48,41 +60,109 @@ Find me on:
 Change Log:
 V1.00, 13/11/2014 - Initial version
 V1.01, 26/06/2015 - Added MAPI/HTTP URL configuration
+V1.02, 27/08/2015 - Improved error handling, can now specify multiple servers to configure at once.
 #>
 
 #requires -version 2
 
+[CmdletBinding()]
 param(
-	[Parameter( Mandatory=$true)]
-	[string]$Server,
+	[Parameter( Position=0,Mandatory=$true)]
+	[string[]]$Server,
 
 	[Parameter( Mandatory=$true)]
 	[string]$InternalURL,
 
 	[Parameter( Mandatory=$true)]
-	[string]$ExternalURL
+	[string]$ExternalURL,
+
+    [Parameter( Mandatory=$false)]
+    [string]$DefaultAuth="NTLM",
+
+    [Parameter( Mandatory=$false)]
+    [Boolean]$InternalSSL=$true,
+
+    [Parameter( Mandatory=$false)]
+    [Boolean]$ExternalSSL=$true
 	)
 
-Write-Host "Configuring Outlook Anywhere URLs"
-Get-OutlookAnywhere -Server $Server | Set-OutlookAnywhere -ExternalHostname $externalurl -InternalHostname $internalurl -ExternalClientsRequireSsl $true -InternalClientsRequireSsl $true -DefaultAuthenticationMethod NTLM
 
-Write-Host "Configuring Outlook Web App URLs"
-Get-OwaVirtualDirectory -Server $server | Set-OwaVirtualDirectory -ExternalUrl https://$externalurl/owa -InternalUrl https://$internalurl/owa
+#...................................
+# Script
+#...................................
 
-Write-Host "Configuring Exchange Control Panel URLs"
-Get-EcpVirtualDirectory -Server $server | Set-EcpVirtualDirectory -ExternalUrl https://$externalurl/ecp -InternalUrl https://$internalurl/ecp
+Begin {
 
-Write-Host "Configuring ActiveSync URLs"
-Get-ActiveSyncVirtualDirectory -Server $server | Set-ActiveSyncVirtualDirectory -ExternalUrl https://$externalurl/Microsoft-Server-ActiveSync -InternalUrl https://$internalurl/Microsoft-Server-ActiveSync
+    #Add Exchange snapin if not already loaded in the PowerShell session
+    if (Test-Path $env:ExchangeInstallPath\bin\RemoteExchange.ps1)
+    {
+	    . $env:ExchangeInstallPath\bin\RemoteExchange.ps1
+	    Connect-ExchangeServer -auto -AllowClobber
+    }
+    else
+    {
+        Write-Warning "Exchange Server management tools are not installed on this computer."
+        EXIT
+    }
+}
 
-Write-Host "Configuring Exchange Web Services URLs"
-Get-WebServicesVirtualDirectory -Server $server | Set-WebServicesVirtualDirectory -ExternalUrl https://$externalurl/EWS/Exchange.asmx -InternalUrl https://$internalurl/EWS/Exchange.asmx
+Process {
 
-Write-Host "Configuring Offline Address Book URLs"
-Get-OabVirtualDirectory -Server $server | Set-OabVirtualDirectory -ExternalUrl https://$externalurl/OAB -InternalUrl https://$internalurl/OAB
+    foreach ($i in $server)
+    {
+        if ((Get-ExchangeServer $i -ErrorAction SilentlyContinue).IsClientAccessServer)
+        {
+            Write-Host "----------------------------------------"
+            Write-Host " Configuring $i"
+            Write-Host "----------------------------------------`r`n"
+            Write-Host "Values:"
+            Write-Host " - Internal URL: $InternalURL"
+            Write-Host " - External URL: $ExternalURL"
+            Write-Host " - Outlook Anywhere default authentication: $DefaultAuth"
+            Write-Host " - Outlook Anywhere internal SSL required: $InternalSSL"
+            Write-Host " - Outlook Anywhere external SSL required: $ExternalSSL"
+            Write-Host "`r`n"
 
-Write-Host "Configuring MAPI/HTTP URLs"
-Get-MapiVirtualDirectory -Server $server | Set-MapiVirtualDirectory -ExternalUrl https://$externalurl/mapi -InternalUrl https://$internalurl/mapi
+            Write-Host "Configuring Outlook Anywhere URLs"
+            Get-OutlookAnywhere -Server $i | Set-OutlookAnywhere -ExternalHostname $externalurl -InternalHostname $internalurl -ExternalClientsRequireSsl $ExternalSSL -InternalClientsRequireSsl $InternalSSL -DefaultAuthenticationMethod $DefaultAuth
 
-Write-Host "Configuring Autodiscover"
-Get-ClientAccessServer $server | Set-ClientAccessServer -AutoDiscoverServiceInternalUri https://$internalurl/Autodiscover/Autodiscover.xml
+            Write-Host "Configuring Outlook Web App URLs"
+            Get-OwaVirtualDirectory -Server $i | Set-OwaVirtualDirectory -ExternalUrl https://$externalurl/owa -InternalUrl https://$internalurl/owa
+
+            Write-Host "Configuring Exchange Control Panel URLs"
+            Get-EcpVirtualDirectory -Server $i | Set-EcpVirtualDirectory -ExternalUrl https://$externalurl/ecp -InternalUrl https://$internalurl/ecp
+
+            Write-Host "Configuring ActiveSync URLs"
+            Get-ActiveSyncVirtualDirectory -Server $i | Set-ActiveSyncVirtualDirectory -ExternalUrl https://$externalurl/Microsoft-Server-ActiveSync -InternalUrl https://$internalurl/Microsoft-Server-ActiveSync
+
+            Write-Host "Configuring Exchange Web Services URLs"
+            Get-WebServicesVirtualDirectory -Server $i | Set-WebServicesVirtualDirectory -ExternalUrl https://$externalurl/EWS/Exchange.asmx -InternalUrl https://$internalurl/EWS/Exchange.asmx
+
+            Write-Host "Configuring Offline Address Book URLs"
+            Get-OabVirtualDirectory -Server $i | Set-OabVirtualDirectory -ExternalUrl https://$externalurl/OAB -InternalUrl https://$internalurl/OAB
+
+            Write-Host "Configuring MAPI/HTTP URLs"
+            Get-MapiVirtualDirectory -Server $i | Set-MapiVirtualDirectory -ExternalUrl https://$externalurl/mapi -InternalUrl https://$internalurl/mapi
+
+            Write-Host "Configuring Autodiscover"
+            Get-ClientAccessServer $i | Set-ClientAccessServer -AutoDiscoverServiceInternalUri https://$internalurl/Autodiscover/Autodiscover.xml
+
+            Write-Host "`r`n"
+        }
+        else
+        {
+            Write-Host -ForegroundColor Yellow "$i is not a Client Access server."
+        }
+    }
+}
+
+End {
+
+    Write-Host "Finished processing all servers specified. Consider running Get-CASHealthCheck.ps1 to test your Client Access namespace and SSL configuration."
+    Write-Host "Refer to http://exchangeserverpro.com/testing-exchange-server-2013-client-access-server-health-with-powershell/ for more details."
+
+}
+
+#...................................
+# Finished
+#...................................
---
+++ GetExchangeURLs.ps1
@@ -0,0 +1,141 @@
+﻿<#
+.SYNOPSIS
+GetExchangeURLs.ps1
+
+.DESCRIPTION 
+PowerShell script to display the Client Access server URLs
+for Microsoft Exchange Server 2013/2016.
+
+.PARAMETER Server
+The name(s) of the server(s) you want to view the URLs for.
+
+.EXAMPLE
+.\Get-ExchangeURLs.ps1 -Server sydex1
+
+.LINK
+http://exchangeserverpro.com/powershell-script-configure-exchange-urls/
+
+.NOTES
+Written by: Paul Cunningham
+
+For more Exchange Server tips, tricks and news
+check out Exchange Server Pro.
+
+* Website:	http://exchangeserverpro.com
+* Twitter:	http://twitter.com/exchservpro
+
+Find me on:
+
+* My Blog:	http://paulcunningham.me
+* Twitter:	https://twitter.com/paulcunningham
+* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* Github:	https://github.com/cunninghamp
+
+Change Log:
+V1.00, 27/08/2015 - Initial version
+#>
+
+#requires -version 2
+
+
+[CmdletBinding()]
+param(
+	[Parameter( Position=0,Mandatory=$true)]
+	[string[]]$Server
+	)
+
+
+#...................................
+# Script
+#...................................
+
+Begin {
+
+    #Add Exchange snapin if not already loaded in the PowerShell session
+    if (Test-Path $env:ExchangeInstallPath\bin\RemoteExchange.ps1)
+    {
+	    . $env:ExchangeInstallPath\bin\RemoteExchange.ps1
+	    Connect-ExchangeServer -auto -AllowClobber
+    }
+    else
+    {
+        Write-Warning "Exchange Server management tools are not installed on this computer."
+        EXIT
+    }
+}
+
+Process {
+
+    foreach ($i in $server)
+    {
+        if ((Get-ExchangeServer $i -ErrorAction SilentlyContinue).IsClientAccessServer)
+        {
+            Write-Host "----------------------------------------"
+            Write-Host " Querying $i"
+            Write-Host "----------------------------------------`r`n"
+            Write-Host "`r`n"
+
+            $OA = Get-OutlookAnywhere -Server $i -AdPropertiesOnly | Select InternalHostName,ExternalHostName
+            Write-Host "Outlook Anywhere"
+            Write-Host " - Internal: $($OA.InternalHostName)"
+            Write-Host " - External: $($OA.ExternalHostName)"
+            Write-Host "`r`n"
+
+            $OWA = Get-OWAVirtualDirectory -Server $i -AdPropertiesOnly | Select InternalURL,ExternalURL
+            Write-Host "Outlook Web App"
+            Write-Host " - Internal: $($OWA.InternalURL)"
+            Write-Host " - External: $($OWA.ExternalURL)"
+            Write-Host "`r`n"
+
+            $ECP = Get-ECPVirtualDirectory -Server $i -AdPropertiesOnly | Select InternalURL,ExternalURL
+            Write-Host "Exchange Control Panel"
+            Write-Host " - Internal: $($ECP.InternalURL)"
+            Write-Host " - External: $($ECP.ExternalURL)"
+            Write-Host "`r`n"
+
+            $OAB = Get-OABVirtualDirectory -Server $i -AdPropertiesOnly | Select InternalURL,ExternalURL
+            Write-Host "Offline Address Book"
+            Write-Host " - Internal: $($OAB.InternalURL)"
+            Write-Host " - External: $($OAB.ExternalURL)"
+            Write-Host "`r`n"
+
+            $EWS = Get-WebServicesVirtualDirectory -Server $i -AdPropertiesOnly | Select InternalURL,ExternalURL
+            Write-Host "Exchange Web Services"
+            Write-Host " - Internal: $($EWS.InternalURL)"
+            Write-Host " - External: $($EWS.ExternalURL)"
+            Write-Host "`r`n"
+
+            $MAPI = Get-MAPIVirtualDirectory -Server $i -AdPropertiesOnly | Select InternalURL,ExternalURL
+            Write-Host "MAPI"
+            Write-Host " - Internal: $($MAPI.InternalURL)"
+            Write-Host " - External: $($MAPI.ExternalURL)"
+            Write-Host "`r`n"
+
+            $EAS = Get-ActiveSyncVirtualDirectory -Server $i -AdPropertiesOnly | Select InternalURL,ExternalURL
+            Write-Host "ActiveSync"
+            Write-Host " - Internal: $($EAS.InternalURL)"
+            Write-Host " - External: $($EAS.ExternalURL)"
+            Write-Host "`r`n"
+
+            $AutoD = Get-ClientAccessServer $i | Select AutoDiscoverServiceInternalUri
+            Write-Host "Autodiscover"
+            Write-Host " - Internal SCP: $($AutoD.AutoDiscoverServiceInternalUri)"
+            Write-Host "`r`n"
+
+        }
+        else
+        {
+            Write-Host -ForegroundColor Yellow "$i is not a Client Access server."
+        }
+    }
+}
+
+End {
+
+    Write-Host "Finished querying all servers specified."
+
+}
+
+#...................................
+# Finished
+#...................................
